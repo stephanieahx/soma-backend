a1d62c61943ecaf85140052eedaedf0ba87749d0
stephanieahx <stephanieanghs@gmail.com>
2020-08-25 22:25:43 +0800

    pseudo code for user stories in routes.js; set up controller files

---
+++ routes.js
@@ -0,0 +1,24 @@
+const userController = require('./controllers/userController');
+const apiController = require('./controllers/sentimentController');
+const sentimentController = require('./controllers/sentimentController');
+const chartController = require('./controllers/sentimentController');
+
+module.exports = app => {
+
+    // USERS
+    // Register user account
+
+    // Log into user account
+
+    // Delete user account
+
+
+    // SENTIMENT
+    // Search topic on twitter for current (today's) sentiment score
+
+    // Add topic to tracked 
+
+    // TRENDS
+    // Show dashboard of trend charts (daily sentiment scores) of topics saved by user
+    
+}
